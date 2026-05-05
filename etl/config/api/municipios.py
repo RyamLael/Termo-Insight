51622d9229d21a9ceea68f5dcd54036e06ead961
Ryam Lael <ryyamlael@gmail.com>
2026-05-05 14:55:07 -0300

    feat: add a municipio extractor from ibge api

--- etl/config/api/municipios.py
+++ etl/config/api/municipios.py
@@ -5,4 +5,5 @@ class MunicipiosConfig():
     
     # Dados das cidades
     STATE_ID = 23 # Ceará
-    MUNICIPIOS_URL = f"https://servicodados.ibge.gov.br/api/v1/localidades/estados/{STATE_ID}/municipios"
+    MUNICIPIOS_URL = f"https://servicodados.ibge.gov.br/api/v1/localidades/estados/"
+    TIMEOUT = 10
---
+++ etl/extract/municipios.py
@@ -0,0 +1,61 @@
+import requests
+
+from config.api.municipios import MunicipiosConfig
+
+class MunicipiosExtractor:
+
+
+    @staticmethod
+    def fetch_municipios(estado_id:str = MunicipiosConfig.STATE_ID):
+
+        """
+        Faz requisição para obter todos os municípios de um estado.
+
+        Args:
+            estado_id(str): ID do estado usado na requisição
+
+        Returns: 
+            list[dict]: Lista de municipios normalizados
+        """
+
+        url = f"{MunicipiosConfig.BASE_URL}{estado_id}/municipios"
+
+        try:
+            response = requests.get(url, timeout=MunicipiosConfig.TIMEOUT)
+            response.raise_for_status()
+            data = response.json()
+
+        except requests.RequestException as e:
+            raise RuntimeError(f"Erro ao buscar municípios: {e}")
+
+        except ValueError:
+            raise RuntimeError("Erro ao converter resposta para JSON")
+
+        return MunicipiosExtractor.parse_municipios(data)
+    
+    @staticmethod
+    def parse_municipios(data):
+        """
+        Converte a resposta da API em estrutura padronizada.
+
+        Args:
+            data (list): JSON retornado pela API
+
+        Returns:
+            list[dict]: Lista de municípios normalizados
+        """
+
+        municipios = []
+
+        for item in data:
+            try:
+                municipios.append({
+                    "id": item["id"],
+                    "nome": item["nome"],
+                    # ajuste aqui dependendo da estrutura real:
+                    "estado_id": item["UF-id"]
+                })
+            except KeyError as e:
+                raise RuntimeError(f"Erro ao parsear município: campo ausente {e}")
+
+        return municipios
